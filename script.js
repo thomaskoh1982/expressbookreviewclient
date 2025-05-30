7f568603cd611ed3267d962176c39d288e8e4077
thomaskoh1982 <40250470+thomaskoh1982@users.noreply.github.com>
2025-05-31 06:33:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://express-book-reviews-one.vercel.app';
+const API_BASE_URL = 'https://express-book-reviews-two.vercel.app/';
 // DOM Elements
 const sections = {
     'all-books': document.getElementById('all-books'),
